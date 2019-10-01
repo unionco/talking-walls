3f05c4b94599c6362c3dcaafb81881106d1c650d
Chris Weissenberger <chris@Chris-Weissenberger.local>
2019-10-01 10:45:07 -0400

    Year tag styles

--- wp-content/plugins/wall-map/templates/template-map.php
+++ wp-content/plugins/wall-map/templates/template-map.php
@@ -33,9 +33,6 @@ if ($query && count($query->posts) > 0) {
 ?>
 
 <div id="main-content">
-
-<?php if ( ! $is_page_builder_used ) : ?>
-
 	<div class="murals" data-murals='<?php echo json_encode($projectJson); ?>'>
 		<div class="murals-list" data-murals-list>
 			<?php
@@ -46,7 +43,7 @@ if ($query && count($query->posts) > 0) {
 					echo '<div class="murals-list-item">
 						<div class="murals-list-item-image">
 							<img src="' . $featuredImage . '" />
-							<div class="murals-"><span></span> 2019</div>
+							<div class="murals-list-item-tag"><span></span> 2019</div>
 						</div>
 						<div class="murals-list-item-info">
 							<h3>' . $title . '</h3>
@@ -62,59 +59,29 @@ if ($query && count($query->posts) > 0) {
 		
 	<div class="container">
 		<div id="content-area" class="clearfix">
-			<div id="left-area">
-
-<?php endif; ?>
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<?php if ( ! $is_page_builder_used ) : ?>
-
-					<h1 class="entry-title main_title"><?php the_title(); ?></h1>
-				<?php
-					$thumb = '';
-
-					$width = (int) apply_filters( 'et_pb_index_blog_image_width', 1080 );
-
-					$height = (int) apply_filters( 'et_pb_index_blog_image_height', 675 );
-					$classtext = 'et_featured_image';
-					$titletext = get_the_title();
-					$thumbnail = get_thumbnail( $width, $height, $classtext, $titletext, $titletext, false, 'Blogimage' );
-					$thumb = $thumbnail["thumb"];
-
-					if ( 'on' === et_get_option( 'divi_page_thumbnails', 'false' ) && '' !== $thumb )
-						print_thumbnail( $thumb, $thumbnail["use_timthumb"], $titletext, $width, $height );
-				?>
-				<?php endif; ?>
-
-					<div class="entry-content">
-                    <?php
-						the_content();
-
-						if ( ! $is_page_builder_used )
-							wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'Divi' ), 'after' => '</div>' ) );
-					?>
-					</div> <!-- .entry-content -->
-
-				<?php
-					if ( ! $is_page_builder_used && comments_open() && 'on' === et_get_option( 'divi_show_pagescomments', 'false' ) ) comments_template( '', true );
-				?>
-
-				</article> <!-- .et_pb_post -->
-
-			<?php endwhile; ?>
-
-<?php if ( ! $is_page_builder_used ) : ?>
-
-			</div> <!-- #left-area -->
-
+			<div id="left-area"></div> <!-- #left-area -->
 			<?php get_sidebar(); ?>
 		</div> <!-- #content-area -->
 	</div> <!-- .container -->
 
-<?php endif; ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<div class="entry-content">
+			<?php
+				the_content();
+
+				if ( ! $is_page_builder_used )
+					wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'Divi' ), 'after' => '</div>' ) );
+			?>
+			</div> <!-- .entry-content -->
+
+		<?php
+			if ( ! $is_page_builder_used && comments_open() && 'on' === et_get_option( 'divi_show_pagescomments', 'false' ) ) comments_template( '', true );
+		?>
+
+		</article> <!-- .et_pb_post -->
+
+	<?php endwhile; ?>
 
 </div> <!-- #main-content -->
 
